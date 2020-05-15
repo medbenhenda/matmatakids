a96f51cd1766784893c42565eff2c01a44df19f7
Mhemed Ben Henda <benhenda.med@gmail.com>
2020-05-15 02:55:30 +0200

    jenkins: edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-echo 'current directory: $(pwd)'
+echo "current directory: $(pwd)"
 cd /var/www/matmatakids/matmatakids
-echo 'current directory after cd: $(pwd)'
+echo "current directory after cd: $(pwd)"
 mv public/bundles public/media public/node_modules public/receipt public/uploads /var/www/matmatakids/back
 #npm install chart.js --save
 #composer install
