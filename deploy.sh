5c7ee81fa65da0208b9633aba31612623cee0003
Mhemed Ben Henda <benhenda.med@gmail.com>
2020-05-15 03:10:11 +0200

    jenkins: edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,12 @@
 echo "current directory: $(pwd)"
 cd /var/www/matmatakids/matmatakids
 echo "current directory after cd: $(pwd)"
-mv public/bundles public/media public/node_modules public/receipt public/uploads /var/www/matmatakids/back
+if [ -d "public/uploads" ]
+then
+    mv public/bundles public/media public/node_modules public/receipt public/uploads /var/www/matmatakids/back
+else
+    echo "directories not moved because there not existed"
+fi
+
 #npm install chart.js --save
 #composer install
