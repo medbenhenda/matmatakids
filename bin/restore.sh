465cbf050458940e17525322df27c78ea8e9f0f8
Mhemed Ben Henda <benhenda.med@gmail.com>
2020-05-15 03:25:18 +0200

    jenkins: rename deploy.sh, place it in bin and add restore.sh

---
+++ bin/restore.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+echo "current directory: $(pwd)"
+cd /var/www/matmatakids/matmatakids
+echo "current directory after cd: $(pwd)"
+if [ -d "public/uploads" ]
+then
+    mv public/bundles public/media public/node_modules public/receipt public/uploads /var/www/matmatakids/back
+else
+    echo "directories not moved because there not existed"
+fi
+
+#npm install chart.js --save
+#composer install
