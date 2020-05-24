e3b49b318fed0adabe62e1c8927e3e94cc5c24ee
Mhemed Ben Henda <benhenda.med@gmail.com>
2020-05-24 04:49:15 +0200

    fix sh scripts

--- bin/backup.sh
+++ bin/backup.sh
@@ -2,13 +2,32 @@
 echo "current directory: $(pwd)"
 cd /var/www/matmatakids/matmatakids
 echo "current directory after cd: $(pwd)"
-if [ -d "public/uploads" ]
-then
-    mv public/receipt/* /var/www/matmatakids/back/receipt
-    mv public/subventions/* /var/www/matmatakids/back/subventions
-    mv public/uploads/* /var/www/matmatakids/back/uploads
+
+
+
+DIRU="/var/www/matmatakids/back/uploads"
+DIRR="/var/www/matmatakids/back/receipt"
+DIRS="/var/www/matmatakids/back/subventions"
+
+#uploadss
+if [ "$(ls -A $DIRU)" ]; then
+     mv public/uploads/* /var/www/matmatakids/back/uploads
 else
-    echo "directories not moved because there not existed"
+    echo "$DIRU is Empty"
+fi
+
+#receipt
+if [ "$(ls -A $DIRR)" ]; then
+     mv public/receipt/* /var/www/matmatakids/back/receipt
+else
+    echo "$DIRR is Empty"
+fi
+
+#subventions
+if [ "$(ls -A $DIRS)" ]; then
+     mv public/subventions/* /var/www/matmatakids/back/subventions
+else
+    echo "$DIRS is Empty"
 fi
 
 #npm install chart.js --save
--- bin/restore.sh
+++ bin/restore.sh
@@ -1,15 +1,34 @@
 #!/usr/bin/env bash
 echo "current directory: $(pwd)"
 cd /var/www/matmatakids/matmatakids
+DIRU="/var/www/matmatakids/back/uploads"
+DIRR="/var/www/matmatakids/back/receipt"
+DIRS="/var/www/matmatakids/back/subventions"
 echo "current directory after cd: $(pwd)"
-if [ -d "/var/www/matmatakids/back/uploads" ]
-then
-    mv  /var/www/matmatakids/back/receipt/* public/receipt
-    mv  /var/www/matmatakids/back/public/subventions public/subventions
-    mv  /var/www/matmatakids/back/uploads public/uploads
-    chmod 777 -R public/subventions public/receipt public/uploads
+
+#uploadss
+if [ "$(ls -A $DIRU)" ]; then
+     mv  /var/www/matmatakids/back/uploads/* public/uploads
+     chmod 777 -R public/uploads
 else
-    echo "directories does not moved because there not existed"
+    echo "$DIRU is Empty"
+fi
+
+#subventions
+if [ "$(ls -A $DIRS)" ]; then
+     mv  /var/www/matmatakids/back/subventions/* public/subventions
+     chmod 777 -R public/subventions
+else
+    echo "$DIRS is Empty"
+fi
+
+#receipt
+if [ "$(ls -A $DIRR)" ]; then
+     mv  /var/www/matmatakids/back/receipt/* public/receipt
+     chmod 777 -R public/receipt
+else
+    echo "$DIRR is Empty"
+
 fi
 
 npm install --prefix ./public chart.js --save
